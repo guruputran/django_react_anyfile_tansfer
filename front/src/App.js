1e6dfb9750b2f8acce2c6cfbfb91e8fbe3da2681
guruputran <gpsmartcodes@gmail.com>
2022-05-23 15:09:44 +0800

    chakraUI & tailwind

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import { Stack, Box } from "@chakra-ui/react";
+import { Button, ButtonGroup, Wrap, WrapItem } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <button className="bg-blue-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded-full">
+        Button
+      </button>
+      <div class="inline-flex">
+        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
+          Prev
+        </button>
+        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
+          Next
+        </button>
+      </div>
+      <Stack direction="column">
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          width="100%"
+          py={12}
+          bgImage="url('https://bit.ly/2Z4KKcF')"
+          bgPosition="center"
+          bgRepeat="no-repeat"
+          mb={2}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ButtonGroup gap="4">
+            <Button colorScheme="whiteAlpha">WhiteAlpha</Button>
+            <Button colorScheme="blackAlpha">BlackAlpha</Button>
+          </ButtonGroup>
+        </Box>
+
+        <Wrap spacing={4}>
+          <WrapItem>
+            <Button colorScheme="gray">Gray</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="red">Red</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="orange">Orange</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="yellow">Yellow</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="green">Green</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="teal">Teal</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="blue">Blue</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="cyan">Cyan</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="purple">Purple</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="pink">Pink</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="linkedin">Linkedin</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="facebook">Facebook</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="messenger">Messenger</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="whatsapp">Whatsapp</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="twitter">Twitter</Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="telegram">Telegram</Button>
+          </WrapItem>
+        </Wrap>
+      </Stack>
+    </>
   );
 }
 
